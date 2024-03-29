bb63b6b1d556583656edcf26b45bf6508e8d8dc2
chirutheboss <85287194+chirutheboss@users.noreply.github.com>
2024-03-29 14:41:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,27 @@
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
+from pyrogram import Client
 
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+# Import configuration based on environment variable
+from sample_config import Config  # Adjust this import as per your actual configuration setup
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, idle
-if __name__ == "__main__" :
-    # Creating essential directories, if they does not exists
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
-    if not os.path.isdir(Config.ADMIN_LOCATION):
-        os.makedirs(Config.ADMIN_LOCATION)
-    if not os.path.isdir(Config.CREDENTIALS_LOCATION):
-        os.makedirs(Config.CREDENTIALS_LOCATION)        
-    plugins = dict(
-        root="plugins"
-    )
-    app = pyrogram.Client(
+if __name__ == "__main__":
+    # Your configuration setup code here
+    
+    async def start():
+        print("Starting bot...")
+        await app.start()
+        await app.stop()
+        await app.start()
+        print("Bot started.")
+
+    app = Client(
         "Mega_Link_Downloader_Bot",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
     )
+
+    app.on_startup(start)
+
     app.run()
-    idle()
+    app.idle()
